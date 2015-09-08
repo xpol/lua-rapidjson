47843e9c2612c38217d6c67bc0fefd6286bdbe02
xpol <xpolife@gmail.com>
2015-09-08 11:58:19 +0800

    Fixes missing !..!

--- .scripts/install.bat
+++ .scripts/install.bat
@@ -169,7 +169,7 @@ if not exist "%LR_ROOT%" (
 	set MSVS_GENERATORS[2013]=Visual Studio 12 2013
 	set MSVS_GENERATORS[2015]=Visual Studio 14 2015
 
-	set CMAKE_GENERATOR=MSVS_GENERATORS[%Configuration%]
+	set CMAKE_GENERATOR=!MSVS_GENERATORS[%Configuration%]!
 
 	if "%platform%" EQU "x64" (set CMAKE_GENERATOR=!CMAKE_GENERATOR! Win64)
 
