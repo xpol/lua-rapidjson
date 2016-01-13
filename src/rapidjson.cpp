73f013a441c326c01a4fb506b7c2fb667c15a880
Xpol Wan <xpolife@gmail.com>
2016-01-13 14:11:48 +0800

    I hate the outdated compilers...

--- src/rapidjson.cpp
+++ src/rapidjson.cpp
@@ -1,5 +1,5 @@
-#include <cstdint>
-#include <limits>
+#define __STDC_LIMIT_MACROS
+#include <stdint>
 #include <cstdio>
 #include <cmath>
 #include <vector>
@@ -395,9 +395,7 @@ private:
         double intpart;
         if (modf(lua_tonumber(L, idx), &intpart) == 0.0)
         {
-            static const double MIN = std::numeric_limits<int64_t>::min();
-            static const double MAX = std::numeric_limits<int64_t>::max();
-            if (MIN <= intpart && intpart <= MAX)
+            if (INT64_MIN <= intpart && intpart <= INT64_MAX)
             {
                 *out = (int64_t)intpart;
                 return true;
