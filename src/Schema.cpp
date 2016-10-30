238bd71a260a39a4348ca9b8b642b4a8122c4f68
Xpol Wan <xpolife@gmail.com>
2016-10-30 16:40:32 +0800

    SchemaValidator:validate() returns 2nd arg as error message and resets validator status.

--- src/Schema.cpp
+++ src/Schema.cpp
@@ -65,55 +65,88 @@ SchemaValidator* Userdata<SchemaValidator>::construct(lua_State * L)
 	return new SchemaValidator(*sd);
 }
 
+/*
 static int SchemaValidator_reset(lua_State* L) {
 	auto validator = Userdata<SchemaValidator>::check(L, 1);
 	validator->Reset();
 	return 0;
+}
+*/
+
+static void pushValidator_error(lua_State* L, SchemaValidator* validator) {
+    luaL_Buffer b;
+    luaL_buffinit(L, &b);
+
+    luaL_addstring(&b, "invalid \"");
+
+    luaL_addstring(&b, validator->GetInvalidSchemaKeyword());
+    luaL_addstring(&b, "\" in docuement at pointer \"");
+
+    // docuement pointer
+    StringBuffer sb;
+    validator->GetInvalidDocumentPointer().StringifyUriFragment(sb);
+    luaL_addlstring(&b, sb.GetString(), sb.GetSize());
+    luaL_addchar(&b, '"');
+
+    luaL_pushresult(&b);
 }
 
 
 static int SchemaValidator_validate(lua_State* L) {
     auto validator = Userdata<SchemaValidator>::check(L, 1);
     auto doc = Userdata<Document>::check(L, 2);
-    lua_pushboolean(L, doc->Accept(*validator));
-    return 1;
+    auto ok = doc->Accept(*validator);
+    lua_pushboolean(L, ok);
+    int nr;
+    if (ok)
+        nr = 1;
+    else
+    {
+        pushValidator_error(L, validator);
+        nr = 2;
+    }
+    validator->Reset();
+
+    return nr;
 }
 
 
 /**
  * local keyword, docpointer, schemapointer = validator:error()
  */
+#if 0
 static int SchemaValidator_error(lua_State* L) {
     auto validator = Userdata<SchemaValidator>::check(L, 1);
     if (validator->IsValid()) {
         lua_pushnil(L);
         return 1;
     }
-        
+
     // keyword
     lua_pushstring(L, validator->GetInvalidSchemaKeyword());
-    
+
     // docuement pointer
     StringBuffer sb;
     validator->GetInvalidDocumentPointer().StringifyUriFragment(sb);
     lua_pushlstring(L, sb.GetString(), sb.GetSize());
-    
-    
+
+
     // schema pointer
     sb.Clear();
     validator->GetInvalidSchemaPointer().StringifyUriFragment(sb);
     lua_pushlstring(L, sb.GetString(), sb.GetSize());
-    
+
     return 3;
 }
+#endif
 
 template <>
 const luaL_Reg* Userdata<SchemaValidator>::methods() {
 	static const luaL_Reg reg[] = {
 		{ "__gc", metamethod_gc },
-		{ "reset", SchemaValidator_reset },
+		// { "reset", SchemaValidator_reset },
         { "validate", SchemaValidator_validate },
-        { "error", SchemaValidator_error },
+        // { "error", SchemaValidator_error },
 		{ nullptr, nullptr }
 	};
 	return reg;
