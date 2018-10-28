10c5202dfaa33a876ec6a9163c72301bba594a5c
Xpol Wan <xpolife@gmail.com>
2018-10-28 23:29:50 +0800

    Add test case for issue #22.

--- spec/json_encode_spec.lua
+++ spec/json_encode_spec.lua
@@ -162,5 +162,12 @@ describe('rapidjson.encode()', function()
         ["ko"] = "나는 유리를 먹을 수 있어요. 그래도 아프지 않아요"
       }, {sort_keys=true})
     )
+	end)
+	
+	it('should not convert integer to double #22', function()
+    assert.are.same(
+      '{"id":7}',
+      rapidjson.encode( {id=7} )
+    )
   end)
 end)
